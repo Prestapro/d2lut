bbd2d8ef9df232bea527d6c32befda65d9edeff9
Prestapro <prestapro@users.noreply.github.com>
2026-03-03 14:31:41 +1000

    chore: remove 7857 scraped HTML files from repo, add data/raw/ to .gitignore

--- data/raw/maxroll/d2planner/auto-loader.js
+++
@@ -1,7 +0,0 @@
-import { p as o } from "./loader-4e1c75d7.js";
-document.addEventListener("DOMContentLoaded", () => {
-  for (const e of [100, 300, 500, 1e3, 2e3, 3e3])
-    setTimeout(() => {
-      o();
-    }, e);
-});
